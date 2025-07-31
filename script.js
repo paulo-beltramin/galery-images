88496dc9f816dacdc7e11bb5beaeb0f901a9fa21
paulo-beltramin <dev.beltramin@gmail.com>
2025-07-31 17:37:28 -0300

    funcionalidade de adicionar imagens

--- script.js
+++ script.js
@@ -8,11 +8,20 @@ $("#btn-header").click(function () {
 
 $("#to-close").click(function () {
 
-$("#forms").slideUp()
-        
+    $("#forms").slideUp()
+
 });
 
 
 $("#forms").on("submit", function (e) {
 
+    e.preventDefault()
+    const url = $("#input").val()
+    const newItem = $("<li style ='display:none'></li>")
+    $(`<img src=${url} alt="imagem">`).appendTo(newItem)
+    $(`<a href=${url} target="_blank">Ver imagem</a>`).appendTo(newItem)
+
+    $(newItem).appendTo("#list")
+
+    $(newItem).fadeIn(1000)
 });

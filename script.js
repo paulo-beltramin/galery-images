069969fc384603e8dd81c463b9f091e209c5c8ff
paulo-beltramin <dev.beltramin@gmail.com>
2025-08-02 00:25:27 -0300

    finalizado funcionalidade de adicionar fotos, iniciando responsividade

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ $("#forms").on("submit", function (e) {
 
     e.preventDefault()
     const url = $("#input").val()
+
+
+    if ($("#input").val() === "") {
+        return
+    }
     const newItem = $("<li style ='display:none'></li>")
     $(`<img src=${url} alt="imagem">`).appendTo(newItem)
     $(`<a href=${url} target="_blank">Ver imagem</a>`).appendTo(newItem)
@@ -24,4 +29,8 @@ $("#forms").on("submit", function (e) {
     $(newItem).appendTo("#list")
 
     $(newItem).fadeIn(1000)
+
+    $("#input").val('')
+
+
 });
